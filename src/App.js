0fbdd48d2610f21109cfe3c1eb72352e0f68a0df
ijw9209 <ijw9209@naver.com>
2020-04-10 01:25:55 +0900

    layout building..

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import './reset.css'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  state = {
+    assetsList: []
+  };
+
+  // getAssetNames = async () => {
+  //   try {
+  //     const res = await fetch('/assets')
+  //       .then(res => res.json());
+  //     console.log(res);
+  //     this.setState({
+  //       assetSList: res
+  //     })
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+
+  // }
+  // componentDidMount() {
+  //   this.getAssetNames();
+  // }
+  render() {
+    return (
+      <div className="App">
+        <div className="container">
+          <div className="row tab_menu">
+            <div className="col-10 mx-auto col-md-8 mt-4">
+              1 col
+            </div>
+          </div>
+          <div className="row main_menu mt-4">
+            <div className="col main_menu">
+              2 col
+              </div>
+          </div>
+        </div>
+
+      </div>
+    );
+  }
 }
 
 export default App;
